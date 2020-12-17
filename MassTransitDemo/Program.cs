1f0d1ca4cfbda6a5c58efcf7da00f6b345be52e6
linh.nguyen <lightalakanzam@gmail.com>
2020-12-18 02:15:25 +0700

    - Done first demo (pub/sub in the same application)

--- MassTransitDemo/Program.cs
+++ MassTransitDemo/Program.cs
@@ -6,61 +6,71 @@ using MassTransit;
 using MassTransit.RabbitMqTransport;
 using MassTransitDemo.Services;
 using Microsoft.Extensions.DependencyInjection;
+using MTransit.Messages.Models;
 
 namespace MassTransitDemo
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			var serviceProviderFactory = new DefaultServiceProviderFactory();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProviderFactory = new DefaultServiceProviderFactory();
 
-			var services = new ServiceCollection();
-			var contexts =
-				typeof(CommonMessageConsumer)
-					.Assembly
-					.GetTypes()
-					.Where(x => typeof(IConsumer).IsAssignableFrom(x) && !x.IsInterface)
-					.ToArray();
+            var services = new ServiceCollection();
+            var contexts =
+                typeof(CommonMessageConsumer)
+                    .Assembly
+                    .GetTypes()
+                    .Where(x => typeof(IConsumer).IsAssignableFrom(x) && !x.IsInterface)
+                    .ToArray();
 
-			foreach (var context in contexts)
-				services.AddSingleton(typeof(IConsumer), context);
+            //foreach (var context in contexts)
+            //	services.AddSingleton(typeof(IConsumer<>), context);
+            services.AddSingleton<IConsumer<CommonMessage>, CommonMessageConsumer>();
 
-			var serviceProvider = serviceProviderFactory.CreateServiceProvider(services);
+            var serviceProvider = serviceProviderFactory.CreateServiceProvider(services);
 
-			var commonMessageQueueName = "common-message";
-			
-			var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
-			{
-				//var connectionString = $"amqp://admin:admin123@10.16.114.42:15672/";
+            var commonMessageQueueName = "common-message";
 
-				cfg.Host(new Uri("amqp://10.16.114.42:5672/"), configurator =>
-				{
-					configurator.Username("admin");
-					configurator.Password("admin123");
-				});
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                //var connectionString = $"amqp://admin:admin123@10.16.114.42:15672/";
 
-				cfg.ReceiveEndpoint(commonMessageQueueName, e =>
-				{
-					e.Durable = true;
-					e.Exclusive = false;
-					e.AutoDelete = true;
+                cfg.Host(new Uri("amqps://mustang.rmq.cloudamqp.com/vjqpxaxw"), configurator =>
+                {
+                    configurator.Username("vjqpxaxw");
+                    configurator.Password("n8WSzZNJEl3Oi2L8RvHUFK_n0nwTl2BN");
+                });
 
-					// // delegate consumer factory
-					// e.Consumer(() => new SubmitOrderConsumer());
-					//
-					// // another delegate consumer factory, with dependency
-					// e.Consumer(() => new LogOrderSubmittedConsumer(Console.Out));
+                cfg.ReceiveEndpoint(commonMessageQueueName, e =>
+                {
+                    e.Durable = true;
+                    e.Exclusive = false;
+                    e.AutoDelete = true;
 
-					// a type-based factory that returns an object (specialized uses)
-					var consumerType = typeof(CommonMessageConsumer);
-					e.Consumer(consumerType, type => serviceProvider.GetService(type));
-				});
-			});
+                    // // delegate consumer factory
+                    // e.Consumer(() => new SubmitOrderConsumer());
+                    //
+                    // // another delegate consumer factory, with dependency
+                    // e.Consumer(() => new LogOrderSubmittedConsumer(Console.Out));
 
-			busControl.StartAsync().Wait();
-			Console.WriteLine("MassTransit has been started");
-			Console.ReadLine();
-		}
-	}
+                    // a type-based factory that returns an object (specialized uses)
+                    var consumerType = typeof(IConsumer<CommonMessage>);
+                    e.Consumer(() => (IConsumer<CommonMessage>)serviceProvider.GetService(consumerType));
+
+                });
+            });
+
+            busControl.StartAsync().Wait();
+            Console.WriteLine("MassTransit has been started");
+
+            var message = new CommonMessage();
+            message.Name = "Name 001";
+            message.Description = "Description 001";
+            busControl.Publish(message).Wait();
+            Console.WriteLine("MassTransit has been sent");
+
+            Console.ReadLine();
+        }
+    }
 }
--- MessagePublisher/Program.cs
+++ MessagePublisher/Program.cs
@@ -14,24 +14,32 @@ namespace MessagePublisher
 
 			var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
 			{
-				cfg.Host("rabbit@CPP00089015D", "/", configurator =>
-				{
-					configurator.Username("admin");
-					configurator.Password("admin@123");
-				});
+                cfg.Host(new Uri("amqps://mustang.rmq.cloudamqp.com/vjqpxaxw"), configurator =>
+                {
+                    configurator.Username("vjqpxaxw");
+                    configurator.Password("n8WSzZNJEl3Oi2L8RvHUFK_n0nwTl2BN");
+                });
 
-				cfg.ReceiveEndpoint(commonMessageQueueName, e =>
-				{
-					e.Durable = true;
-					e.Exclusive = false;
-					e.AutoDelete = true;
-				});
+                cfg.ReceiveEndpoint(commonMessageQueueName, e =>
+                {
+                    e.Durable = true;
+                    e.Exclusive = false;
+                    e.AutoDelete = true;
+
+                    // // delegate consumer factory
+                    // e.Consumer(() => new SubmitOrderConsumer());
+                    //
+                    // // another delegate consumer factory, with dependency
+                    // e.Consumer(() => new LogOrderSubmittedConsumer(Console.Out));
+                });
 			});
+
+            busControl.StartAsync().Wait();
 
 			var message = new CommonMessage();
 			message.Name = "Name 001";
 			message.Description = "Description 001";
-			busControl.Publish(busControl);
+            busControl.Publish(message).Wait();
 
 			Console.WriteLine("MassTransit publisher has been started");
 			Console.ReadLine();
