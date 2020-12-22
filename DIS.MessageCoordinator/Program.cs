d75f33e8927fddd90be358a2ebef12d4c79f728d
Nguyen Duy Linh <Linh Nguyen>
2020-12-22 10:36:59 +0700

    - Added SqlAuditStore to SendAudit.

--- DIS.MessageCoordinator/Program.cs
+++ DIS.MessageCoordinator/Program.cs
@@ -13,103 +13,104 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace DIS.MessageCoordinator
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var serviceProviderFactory = new DefaultServiceProviderFactory();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var serviceProviderFactory = new DefaultServiceProviderFactory();
 
-            var services = new ServiceCollection();
-            var availablePastEvents =
-                typeof(EbcDeclarationCreatedEvent)
-                    .Assembly
-                    .GetTypes()
-                    .Where(x => typeof(IPastEvent).IsAssignableFrom(x) && !x.IsInterface)
-                    .ToArray();
+			var services = new ServiceCollection();
+			var availablePastEvents =
+				typeof(EbcDeclarationCreatedEvent)
+					.Assembly
+					.GetTypes()
+					.Where(x => typeof(IPastEvent).IsAssignableFrom(x) && !x.IsInterface)
+					.ToArray();
 
-            var availableConsumerTypes = typeof(EbcDeclarationCreatedEventConsumer).Assembly
-                .GetTypes()
-                .Where(x => typeof(IConsumer).IsAssignableFrom(x) && !x.IsInterface)
-                .ToArray();
+			var availableConsumerTypes = typeof(EbcDeclarationCreatedEventConsumer).Assembly
+				.GetTypes()
+				.Where(x => typeof(IConsumer).IsAssignableFrom(x) && !x.IsInterface)
+				.ToArray();
 
-            services.AddEventProcessors<EbcDeclarationCreatedEvent>(
-                typeof(EbcDeclarationCreatedEventProcessor1).Assembly);
+			services.AddEventProcessors<EbcDeclarationCreatedEvent>(
+				typeof(EbcDeclarationCreatedEventProcessor1).Assembly);
 
-            foreach (var availablePastEvent in availablePastEvents)
-            {
-                var genericConsumerType = typeof(IConsumer<>).MakeGenericType(availablePastEvent);
+			foreach (var availablePastEvent in availablePastEvents)
+			{
+				var genericConsumerType = typeof(IConsumer<>).MakeGenericType(availablePastEvent);
 
-                var availableConsumerType = availableConsumerTypes
-                    .FirstOrDefault(x => genericConsumerType.IsAssignableFrom(x));
+				var availableConsumerType = availableConsumerTypes
+					.FirstOrDefault(x => genericConsumerType.IsAssignableFrom(x));
 
-                if (availableConsumerType != null)
-                    services.AddSingleton(genericConsumerType, availableConsumerType);
-            }
+				if (availableConsumerType != null)
+					services.AddSingleton(genericConsumerType, availableConsumerType);
+			}
 
-            // Add event store.
-            services.AddSingleton<IMessageAuditStore, SqlConsumeAuditStore>();
+			// Add event store.
+			services.AddSingleton<IMessageAuditStore, SqlConsumeAuditStore>();
 
-            // Build the configuration
-            var config = new ConfigurationBuilder()
-                .AddUserSecrets<Program>()
-                .Build();
+			// Build the configuration
+			var config = new ConfigurationBuilder()
+				.AddUserSecrets<Program>()
+				.Build();
 
-            var secretProvider = config.Providers.First();
-            services.AddSingleton(provider =>
-            {
-                return Bus.Factory.CreateUsingRabbitMq(cfg =>
-                {
-                    secretProvider.TryGet("RabbitMq:Uri", out var uri);
-                    secretProvider.TryGet("RabbitMq:Username", out var username);
-                    secretProvider.TryGet("RabbitMq:Password", out var password);
+			var secretProvider = config.Providers.First();
+			services.AddSingleton(provider =>
+			{
+				return Bus.Factory.CreateUsingRabbitMq(cfg =>
+				{
+					secretProvider.TryGet("RabbitMq:Uri", out var uri);
+					secretProvider.TryGet("RabbitMq:Username", out var username);
+					secretProvider.TryGet("RabbitMq:Password", out var password);
 
-                    secretProvider.TryGet("RabbitMq:Queue:Name", out var szQueueName);
-                    secretProvider.TryGet("RabbitMq:Queue:Durable", out var szDurable);
-                    secretProvider.TryGet("RabbitMq:Queue:Exclusive", out var szExclusive);
-                    secretProvider.TryGet("RabbitMq:Queue:AutoDelete", out var szAutoDelete);
+					secretProvider.TryGet("RabbitMq:Queue:Name", out var szQueueName);
+					secretProvider.TryGet("RabbitMq:Queue:Durable", out var szDurable);
+					secretProvider.TryGet("RabbitMq:Queue:Exclusive", out var szExclusive);
+					secretProvider.TryGet("RabbitMq:Queue:AutoDelete", out var szAutoDelete);
 
-                    bool.TryParse(szDurable, out var durable);
-                    bool.TryParse(szExclusive, out var exclusive);
-                    bool.TryParse(szAutoDelete, out var autoDelete);
+					bool.TryParse(szDurable, out var durable);
+					bool.TryParse(szExclusive, out var exclusive);
+					bool.TryParse(szAutoDelete, out var autoDelete);
 
-                    cfg.Host(new Uri(uri), configurator =>
-                    {
-                        configurator.Username(username);
-                        configurator.Password(password);
-                    });
+					cfg.Host(new Uri(uri), configurator =>
+					{
+						configurator.Username(username);
+						configurator.Password(password);
+					});
 
-                    cfg.ReceiveEndpoint(szQueueName, e =>
-                    {
-                        e.Durable = durable;
-                        e.Exclusive = exclusive;
-                        e.AutoDelete = autoDelete;
+					cfg.ReceiveEndpoint(szQueueName, e =>
+					{
+						e.Durable = durable;
+						e.Exclusive = exclusive;
+						e.AutoDelete = autoDelete;
 
-                        foreach (var availablePastEvent in availablePastEvents)
-                        {
-                            var genericConsumerType = typeof(IConsumer<>).MakeGenericType(availablePastEvent);
-                            e.Consumer(genericConsumerType, type =>
-                            {
-                                var consumerServices = provider.GetServices(type);
-                                var consumerService = consumerServices.FirstOrDefault();
-                                return consumerService;
-                            });
-                        }
-                    });
-                });
-            });
+						foreach (var availablePastEvent in availablePastEvents)
+						{
+							var genericConsumerType = typeof(IConsumer<>).MakeGenericType(availablePastEvent);
+							e.Consumer(genericConsumerType, type =>
+							{
+								var consumerServices = provider.GetServices(type);
+								var consumerService = consumerServices.FirstOrDefault();
+								return consumerService;
+							});
+						}
+					});
+				});
+			});
 
-            services.AddSingleton<IBus>(provider => provider.GetService<IBusControl>());
+			services.AddSingleton<IBus>(provider => provider.GetService<IBusControl>());
 
-            var serviceProvider = serviceProviderFactory.CreateServiceProvider(services);
-            Console.WriteLine("[DIS.MessageCoordinator] starting");
+			var serviceProvider = serviceProviderFactory.CreateServiceProvider(services);
+			Console.WriteLine("[DIS.MessageCoordinator] starting");
 
-            var busControl = serviceProvider.GetService<IBusControl>();
-            busControl.ConnectConsumeAuditObserver(serviceProvider.GetService<IMessageAuditStore>());
+			var busControl = serviceProvider.GetService<IBusControl>();
+			busControl.ConnectConsumeAuditObserver(serviceProvider.GetService<IMessageAuditStore>());
+			busControl.ConnectSendAuditObservers(serviceProvider.GetService<IMessageAuditStore>());
 
-            busControl.StartAsync().Wait();
-            Console.WriteLine("[DIS.MessageCoordinator] started");
+			busControl.StartAsync().Wait();
+			Console.WriteLine("[DIS.MessageCoordinator] started");
 
-            Console.ReadLine();
-        }
-    }
+			Console.ReadLine();
+		}
+	}
 }
